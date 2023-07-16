8bbf6b50a716736376549218c4f1c528a21dc0c3
Timur <akkord3034@gmail.com>
2023-07-16 16:28:46 +0500

    Добавляет маршрутизацию, компонент Logo, приват. маршрут и react-helmet

--- src/Pages/Login-screen/Login-screen.tsx
+++ src/Pages/Login-screen/Login-screen.tsx
@@ -1,19 +1,17 @@
-function LoginPage(): JSX.Element {
+import {Helmet} from 'react-helmet-async';
+import Logo from '../../Components/Logo/Logo';
+
+function LoginScreen(): JSX.Element {
   return (
     <div className="page page--gray page--login">
+      <Helmet>
+        <title>6 cities: authorization</title>
+      </Helmet>
       <header className="header">
         <div className="container">
           <div className="header__wrapper">
             <div className="header__left">
-              <a className="header__logo-link" href="main.html">
-                <img
-                  className="header__logo"
-                  src="img/logo.svg"
-                  alt="6 cities logo"
-                  width={81}
-                  height={41}
-                />
-              </a>
+              <Logo />
             </div>
           </div>
         </div>
@@ -64,4 +62,4 @@ function LoginPage(): JSX.Element {
   );
 }
 
-export default LoginPage;
+export default LoginScreen;
--- src/Pages/Main-screen/Main-screen.tsx
+++ src/Pages/Main-screen/Main-screen.tsx
@@ -1,19 +1,22 @@
+import {Helmet} from 'react-helmet-async';
 import Card from '../../Components/Card/Card';
+import Logo from '../../Components/Logo/Logo';
 
-type MainPageProps = {
+type MainScreenProps = {
   cardsCount: number;
 }
 
-function MainPage({cardsCount}: MainPageProps): JSX.Element {
+function MainScreen({cardsCount}: MainScreenProps): JSX.Element {
   return (
     <div className="page page--gray page--main">
+      <Helmet>
+        <title>6 cities</title>
+      </Helmet>
       <header className="header">
         <div className="container">
           <div className="header__wrapper">
             <div className="header__left">
-              <a className="header__logo-link header__logo-link--active">
-                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
-              </a>
+              <Logo />
             </div>
             <nav className="header__nav">
               <ul className="header__nav-list">
@@ -112,4 +115,4 @@ function MainPage({cardsCount}: MainPageProps): JSX.Element {
   );
 }
 
-export default MainPage;
+export default MainScreen;
